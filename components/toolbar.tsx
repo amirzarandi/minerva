35575b2a68003a6b2c673924a5b8276d4a0d2f6d
Amir Zarandi <90559838+amirzarandi@users.noreply.github.com>
2023-10-21 16:19:30 -0400

    minor fixes

--- components/toolbar.tsx
+++ components/toolbar.tsx
@@ -9,8 +9,6 @@ import { Doc } from "@/convex/_generated/dataModel";
 import { Button } from "@/components/ui/button";
 import { api } from "@/convex/_generated/api";
 
-import { IconPicker } from "./icon-picker";
-
 interface ToolbarProps {
   initialData: Doc<"documents">;
   preview?: boolean;
@@ -56,30 +54,11 @@ export const Toolbar = ({
     }
   };
 
-  const onIconSelect = (icon: string) => {
-    update({
-      id: initialData._id,
-      icon,
-    });
-  };
-
-  const onRemoveIcon = () => {
-    removeIcon({
-      id: initialData._id
-    })
-  }
-
   return (
     <div className="pl-[54px] group relative">
       {!!initialData.icon && !preview && (
         <div className="flex items-center gap-x-2 group/icon pt-6">
-          <IconPicker onChange={onIconSelect}>
-            <p className="text-6xl hover:opacity-75 transition">
-              {initialData.icon}
-            </p>
-          </IconPicker>
           <Button
-            onClick={onRemoveIcon}
             className="rounded-full opacity-0 group-hover/icon:opacity-100 transition text-muted-foreground text-xs"
             variant="outline"
             size="icon"
@@ -94,18 +73,6 @@ export const Toolbar = ({
         </p>
       )}
       <div className="opacity-0 group-hover:opacity-100 flex items-center gap-x-1 py-4">
-        {!initialData.icon && !preview && (
-          <IconPicker asChild onChange={onIconSelect}>
-            <Button
-              className="text-muted-foreground text-xs"
-              variant="outline"
-              size="sm"
-            >
-              <Smile className="h-4 w-4 mr-2" />
-              Add icon
-            </Button>
-          </IconPicker>
-        )}
         {!initialData.coverImage && !preview && (
           <Button
             className="text-muted-foreground text-xs"
