b19b147ebb64a74f8696e9c367b8bbf646ed7b9b
Amir Zarandi <90559838+amirzarandi@users.noreply.github.com>
2023-10-21 15:17:59 -0400

    added editor

--- components/editor.tsx
+++ components/editor.tsx
@@ -1,5 +1,16 @@
 "use client";
 
+import { useTheme } from "next-themes";
+import {
+  BlockNoteEditor,
+  PartialBlock
+} from "@blocknote/core";
+import {
+  BlockNoteView,
+  useBlockNote
+} from "@blocknote/react";
+import "@blocknote/core/style.css";
+
 interface EditorProps {
   onChange: (value: string) => void;
   initialContent?: string;
@@ -11,10 +22,25 @@ const Editor = ({
   initialContent,
   editable
 }: EditorProps) => {
+  const { resolvedTheme } = useTheme();
+
+  const editor: BlockNoteEditor = useBlockNote({
+    editable,
+    initialContent:
+      initialContent
+      ? JSON.parse(initialContent) as PartialBlock[]
+      : undefined,
+    onEditorContentChange: (editor) => {
+      onChange(JSON.stringify(editor.topLevelBlocks, null, 2));
+    }
+  })
 
   return (
     <div>
-Hi
+      <BlockNoteView
+        editor={editor}
+        theme={resolvedTheme === "dark" ? "dark" : "light"}
+      />
     </div>
   )
 }
