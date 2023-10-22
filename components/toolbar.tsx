0a7d518465fb81258532dc6c929c6ff7f08f2f77
Amir Zarandi <90559838+amirzarandi@users.noreply.github.com>
2023-10-21 22:21:13 -0400

    open-ai integration

--- components/toolbar.tsx
+++ components/toolbar.tsx
@@ -5,9 +5,12 @@ import { ImageIcon, Smile, X } from "lucide-react";
 import { useMutation } from "convex/react";
 import TextareaAutosize from "react-textarea-autosize";
 
+import { useRecording } from "@/hooks/use-recording";
 import { Doc } from "@/convex/_generated/dataModel";
 import { Button } from "@/components/ui/button";
 import { api } from "@/convex/_generated/api";
+
+import { IconPicker } from "./icon-picker";
 
 interface ToolbarProps {
   initialData: Doc<"documents">;
@@ -24,6 +27,8 @@ export const Toolbar = ({
 
   const update = useMutation(api.documents.update);
   const removeIcon = useMutation(api.documents.removeIcon);
+
+  const recording = useRecording();
 
   const enableInput = () => {
     if (preview) return;
@@ -54,11 +59,30 @@ export const Toolbar = ({
     }
   };
 
+  const onIconSelect = (icon: string) => {
+    update({
+      id: initialData._id,
+      icon,
+    });
+  };
+
+  const onRemoveIcon = () => {
+    removeIcon({
+      id: initialData._id
+    })
+  }
+
   return (
     <div className="pl-[54px] group relative">
       {!!initialData.icon && !preview && (
         <div className="flex items-center gap-x-2 group/icon pt-6">
+          <IconPicker onChange={onIconSelect}>
+            <p className="text-6xl hover:opacity-75 transition">
+              {initialData.icon}
+            </p>
+          </IconPicker>
           <Button
+            onClick={onRemoveIcon}
             className="rounded-full opacity-0 group-hover/icon:opacity-100 transition text-muted-foreground text-xs"
             variant="outline"
             size="icon"
@@ -73,8 +97,21 @@ export const Toolbar = ({
         </p>
       )}
       <div className="opacity-0 group-hover:opacity-100 flex items-center gap-x-1 py-4">
+        {!initialData.icon && !preview && (
+          <IconPicker asChild onChange={onIconSelect}>
+            <Button
+              className="text-muted-foreground text-xs"
+              variant="outline"
+              size="sm"
+            >
+              <Smile className="h-4 w-4 mr-2" />
+              Add icon
+            </Button>
+          </IconPicker>
+        )}
         {!initialData.coverImage && !preview && (
           <Button
+            onClick={recording.onOpen}
             className="text-muted-foreground text-xs"
             variant="outline"
             size="sm"
